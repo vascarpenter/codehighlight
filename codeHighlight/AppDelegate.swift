800bcc6aec6dc1da30c7526bf4d0a2bdf06f60cf
vascarpenter <gikoha5666@gmail.com>
2020-04-07 13:31:35 +0900

    Initial Commit

---
+++ codeHighlight/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  codeHighlight
+//
+//  Created by Namikare Gikoha on 2020/04/07.
+//  Copyright © 2020 gikoha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ codeHighlight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  codeHighlight
+//
+//  Created by Namikare Gikoha on 2020/04/07.
+//  Copyright © 2020 gikoha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
